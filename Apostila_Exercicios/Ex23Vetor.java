89e4426b4e5cc1a38ee497ff3b770c7bb30f4eda
Lucas Alencar <78982351+Mrrobot-SP@users.noreply.github.com>
2021-06-18 16:18:13 -0300

    Add files via upload

---
+++ Apostila_Exercicios/Ex23Vetor.java
@@ -0,0 +1,20 @@
+package exerciciosApostila2007;
+
+public class Ex23Vetor {
+	public static void main(String[] args) {
+		String [] pais = {"Brasil","Estados unidos","Alemanha",
+				"Rússia","África","Japão","China",
+				"Argentina","Bolivia","França"};
+		
+		
+		System.out.println(pais[7]);
+		System.out.println(pais[5]);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Apostila_Exercicios/Ex24Matriz.java
@@ -0,0 +1,15 @@
+package exerciciosApostila2007;
+
+public class Ex24Matriz {
+
+	public static void main(String[] args) {
+		
+		int[][] numero={{25,26,27,28},{10,11,14,15}};
+		for(int i=0;i<numero.length;++i){
+          for (int j=0;j<numero[i].length;++j) {
+        	  System.out.println(numero[i][j]);
+          }
+		}
+		
+	}
+}
